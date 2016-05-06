a08e0738be2a72064c5e514d409ec2a378c05ae4
Steve Aitken <steve.aitken@orckestra.com>
2016-05-06 10:46:15 -0400

    Flushed out the intent messages a little more

--- RecipeFinder/RecipeFinder/Dialogs/RecipeFinderDialog.cs
+++ RecipeFinder/RecipeFinder/Dialogs/RecipeFinderDialog.cs
@@ -1,8 +1,9 @@
 ﻿using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Builder.Luis;
-using Microsoft.Bot.Connector;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace RecipeFinder.Dialogs
 {
@@ -10,11 +11,58 @@ namespace RecipeFinder.Dialogs
     [Serializable]
     public class RecipeFinderDialog : LuisDialog<object>
     {
+        #region variables
+
+        public const string Entity_Ingredient = "";
+        public const string Entity_EndProduct = "";
+        public const string Entity_Dietary_Restriction = "";
+
+        #endregion
+
+
+        #region constructors
+
+        public RecipeFinderDialog(ILuisService service = null)
+            : base(service)
+        {
+        }
+
+        #endregion
+
+
         #region public methods
 
         [LuisIntent("FindRecipeByEndProduct")]
         public async Task FindRecipeByEndProduct(IDialogContext context, LuisResult result)
         {
+
+            // check intent score
+            var endProductList = new List<string>();
+            var dietaryRestrictionList = new List<string>();
+            var dietList = new List<string>();
+
+            // get ingredients
+            endProductList.AddRange(result.Entities.Where(f => f.Type == UtteranceType.Ingredient.ToString()).Select(f => f.Entity));
+
+            //not sure if this is correct
+            if (endProductList.Count == 0)
+                await context.PostAsync("Please specify at least one End Product.");
+
+            // check filters
+            dietaryRestrictionList.AddRange(result.Entities.Where(f => f.Type == UtteranceType.DietaryRestriction.ToString()).Select(f => f.Entity));
+            dietList.AddRange(result.Entities.Where(f => f.Type == UtteranceType.Diet.ToString()).Select(f => f.Entity));
+
+
+            // check score
+
+            // get filters
+
+            // if need more info, prompt user for more info
+
+            // make api call?
+
+            // display results based on response
+
             await context.PostAsync("did not find any recipes");
             context.Wait(MessageReceived);
         }
@@ -22,7 +70,35 @@ namespace RecipeFinder.Dialogs
         [LuisIntent("FindRecipeByIngredients")]
         public async Task FindRecipeByIngredients(IDialogContext context, LuisResult result)
         {
+            var ingredientList = new List<string>();
+            var dietaryRestrictionList = new List<string>();
+            var dietList = new List<string>();
+
+            // get ingredients
+            ingredientList.AddRange(result.Entities.Where(f => f.Type == UtteranceType.Ingredient.ToString()).Select(f => f.Entity));
+
+            //not sure if this is correct
+            if (ingredientList.Count == 0)
+                await context.PostAsync("Please specify at least one ingredient.");
+
+            // check filters
+            dietaryRestrictionList.AddRange(result.Entities.Where(f => f.Type == UtteranceType.DietaryRestriction.ToString()).Select(f => f.Entity));
+            dietList.AddRange(result.Entities.Where(f => f.Type == UtteranceType.Diet.ToString()).Select(f => f.Entity));
+
+            // if need more info, prompt user for more info
+
+            // make api call?
+
+            // display results based on response
             await context.PostAsync("did not find any recipes");
+            context.Wait(MessageReceived);
+        }
+
+        [LuisIntent("None")]
+        public async Task None(IDialogContext context, LuisResult result)
+        {
+            string message = $"Sorry I did not understand: " + string.Join(", ", result.Intents.Select(i => i.Intent));
+            await context.PostAsync(message);
             context.Wait(MessageReceived);
         }
 
@@ -35,5 +111,13 @@ namespace RecipeFinder.Dialogs
 
         #endregion
 
+    }
+
+    public enum UtteranceType
+    {
+        EndProduct,
+        Ingredient,
+        DietaryRestriction,
+        Diet
     }
 }
